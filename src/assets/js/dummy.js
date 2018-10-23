db19bfe9221034688943219b37cd65b9bf9ee6e0
Ichsan Wahyudi <ichsan.sano@gmail.com>
2018-10-23 20:17:50 +0700

    adjust style and name, intools part 2 card

--- src/assets/js/dummy.js
+++ src/assets/js/dummy.js
@@ -1,10 +1,11 @@
-var data = [
+var dataChannel = [
   {
     id: 201,
     url: '',
     status: 1,
+    archive: false,
     img: './assets/img/gc1.jpg',
-    title: '2018 FIFA World Cup',
+    name: '2018 FIFA World Cup',
     description: 'Hai para pecinta sepak bola, Ramaikan piala dunia 2018 bersama Tokopedia! Ikuti quiznya dan menangkan Tokocash senilai jutaan rupiah.Buruan, jangan sampai kelewatan!',
     moderator: 'Darius Sinathrya',    
   },
@@ -12,8 +13,9 @@ var data = [
     id: 202,
     url: '',
     status: 1,
+    archive: false,
     img: './assets/img/gc2.jpg',
-    title: 'Pokemon in The 6ix',
+    name: 'Pokemon in The 6ix',
     description: 'Hai para pecinta sepak bola, Ramaikan piala dunia 2018 bersama Tokopedia! Ikuti quiznya dan menangkan Tokocash senilai jutaan rupiah.Buruan, jangan sampai kelewatan!',
     moderator: 'Darius Sinathrya',
   },
@@ -21,15 +23,21 @@ var data = [
     id: 203,
     url: '',
     status: 2,
+    archive: false,
     img: './assets/img/gc1.jpg',
-    title: '2018 Asian Games',
+    name: '2018 Asian Games',
     description: 'Hai para pecinta sepak bola, Ramaikan piala dunia 2018 bersama Tokopedia! Ikuti quiznya dan menangkan Tokocash senilai jutaan rupiah.Buruan, jangan sampai kelewatan!',
     moderator: 'Ichsan Indra Wahyudi',
   }
 ]
 
 $(document).ready(function () {
-    var channelActive = `
+  loopData()
+})
+
+function initContainer() {
+  $('.channel').remove()
+  var channelActive = `
       <div class="content channel channel--active">
               <div class="channel__header">
                 <div class="channel__header-title">
@@ -62,7 +70,7 @@ $(document).ready(function () {
             </div>
     `
 
-    var channelInactive = `
+  var channelInactive = `
       <div class="content channel channel--inactive">
               <div class="channel__header">
                 <div class="channel__header-title">
@@ -95,33 +103,34 @@ $(document).ready(function () {
             </div>
     `
 
-    $('.container').append(channelActive)
-    $('.container').append(channelInactive)
-    loopData()
-})
+  $('.container').append(channelActive)
+  $('.container').append(channelInactive)
+}
 
 function loopData() {
-  for (const key in data) {
+  initContainer()
+  $('.channel__list tbody').empty()
+  for (const data of dataChannel) {
     var listChannel = `
         <tr>
           <td class="channel__list-num">
-            <h6 class="list-num__id">ID ${data[key].id}</h6>
+            <h6 class="list-num__id">ID ${data.id}</h6>
             <span class="list-num__url">(URL budCuv)</span>
           </td>
           <td class="channel__list-gc">
             <div class="list-gc">
-              <img class="list-gc__photo" src="${data[key].img}" alt="">
+              <img class="list-gc__photo" src="${data.img}" alt="">
               <div class="list-gc__text">
-                <h3 class="list-gc__text-title">${data[key].title}</h3>
-                <p class="list-gc__text-desc">${data[key].description}</p>
-                <p class="list-gc__text-moderator">Moderator: <span>${data[key].moderator}</span></p>
+                <h3 class="list-gc__text-title">${data.name}</h3>
+                <p class="list-gc__text-desc">${data.description}</p>
+                <p class="list-gc__text-moderator">Moderator: <span>${data.moderator}</span></p>
               </div>
             </div>
           </td>
           <td class="channel__list-status">
-            <select class="channel__status-select" onchange="changeStatus(this)">
-              <option value="Active" ${data[key].status === 1 ? 'selected' : ''}>Active</option>
-              <option value="Inactive" ${data[key].status === 2 ? 'selected' : ''}>Inactive</option>
+            <select class="channel__status-select" onchange="changeStatus(this, ${data.id})">
+              <option value="Active" ${data.status === 1 ? 'selected' : ''}>Active</option>
+              <option value="Inactive" ${data.status === 2 ? 'selected' : ''}>Inactive</option>
             </select>
           </td>
           <td class="channel__list-action">
@@ -137,20 +146,24 @@ function loopData() {
               <div class="list-action__btn">
                 <a class="unf-user-btn unf-user-btn--small group-chat__btn-action group-chat__btn--chat"><span>chat</span></a>
                 <a class="unf-user-btn unf-user-btn--small group-chat__btn-action group-chat__btn--edit"><span>edit</span></a>
-                <a class="unf-user-btn unf-user-btn--small group-chat__btn-action group-chat__btn--preview" onclick="preview(${data[key].id})"><span>preview</span></a>
-                <a class="unf-user-btn unf-user-btn--small group-chat__btn-action group-chat__btn--archive" onclick="archive(${data[key].id})"><span>archive</span></a>
+                <a class="unf-user-btn unf-user-btn--small group-chat__btn-action group-chat__btn--preview" onclick="preview(${data.id})"><span>preview</span></a>
+                <a class="unf-user-btn unf-user-btn--small group-chat__btn-action group-chat__btn--archive" onclick="archive(${data.id})"><span>archive</span></a>
               </div>
             </div>
           </td>
         </tr>
       `
 
-    if (data[key].status === 1) {
-      $('.channel--active .channel__list tbody').append(listChannel)
-    } else {
-      $('.channel--inactive .channel__list tbody').append(listChannel)
-    }
+    if(!data.archive){
+      if (data.status === 1) {
+        $('.channel--active .channel__list tbody').append(listChannel)
+      } else {
+        $('.channel--inactive .channel__list tbody').append(listChannel)
+      }
+    } 
   }
+
+  initCustomSelect()
 }
 
 function preview(id) {
@@ -159,14 +172,95 @@ function preview(id) {
 
 function archive(id) {
   handleDialogOpen($('.unf-user-dialog--archive-gc'));
+  $('#btn-archive-channel').attr('data-id', id)
 }
 
-function changeStatus(e) {
-  console.log(e.selectedIndex)
+function changeStatus(e, id) {
   if(e.selectedIndex === 0){
     handleDialogOpen($('.unf-user-dialog--activate-gc'));
+    $('#btn-activate-channel').attr('data-id', id)
   }else{
-    handleDialogOpen($('.unf-user-dialog--deactive-gc'));
+    handleDialogOpen($('.unf-user-dialog--deactive-gc')); 
+    $('#btn-deactive-channel').attr('data-id', id)
   }
-  
+}
+
+$(function activateChannel() {
+  $('#btn-activate-channel').on({
+    click: () => {
+      handleStatusChannel(this , 1)
+    }
+  })
+})
+
+$(function deactiveChannel(){
+  $('#btn-deactive-channel').on({
+    click: () => {
+      handleStatusChannel(this , 2)
+    }
+  })
+})
+
+$(function archiveChannel() {
+  $('#btn-archive-channel').on({
+    click: () => {
+      handleArchiveChannel(this, true)
+    }
+  })
+})
+
+$(function handleCreateChannel() {
+  $('#save-create-gc').on({
+    click: () => {
+
+      const id = dataChannel[dataChannel.length - 1].id + 1
+      const name = $('#gc-name').val()
+      const description = $('#gc-desc').val()
+      const moderator = $('#moderator-name').val()
+
+      const newChannel = {
+        id,
+        url: '',
+        status: 1,
+        archive: false,
+        img: './assets/img/gc1.jpg',
+        name,
+        description,
+        moderator
+      }
+
+      pushData(newChannel)
+    }
+  })
+})
+
+function handleStatusChannel(e, val) {
+  var id = $(e).attr('data-id')
+
+  updateData(id, 'status', val)
+}
+
+function handleArchiveChannel(e, val) {
+  var id = $(e).attr('data-id')
+
+  updateData(id, 'archive', val)
+}
+
+// CRUD
+
+function pushData(data) {
+  dataChannel.push(data)
+  handleDialogClose()
+  loopData()
+}
+
+function updateData(id, state, newValue) {
+  for (const data of dataChannel) {
+    if(data.id == id){
+      data[state] = newValue
+      break
+    }
+  }
+  handleDialogClose()
+  loopData()
 }
--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,149 +1,186 @@
-var isCover = false
-var isName = false
-var isDesc = false
-var isModeratorEmail = false
-var isModeratorName = false
+var isCover = false;
+var isName = false;
+var isDesc = false;
+var isModeratorEmail = false;
+var isModeratorName = false;
 
-var email = $('#moderator-email')
-var inputEmail = $('.unf-user-input--moderator-email')
+var email = $('#moderator-email');
+var inputEmail = $('.unf-user-input--moderator-email');
 
-$(document).ready(function () {
-  // init 
+$(document).ready(function() {
+  // init
   $('.customScrollBar').scrollbar();
-  initCustomSelect();
   // handleDialogOpen($('.unf-user-dialog--archive-gc'))
 
   $('.group-chat__btn--create').on({
-    click: function () {
-      handleDialogOpen($('.unf-user-dialog--create-gc'))
-    }
-  })
+    click: () => {
+      handleDialogOpen($('.unf-user-dialog--create-gc'));
+    },
+  });
 
   // Create GC
+  $('.unf-user-dialog__close--create-gc').on({
+    click: () => {
+      // reset input
+      $('#gc-name').val('');
+      $('#gc-desc').val('');
+      $('#moderator-email').val('');
+
+      if ($('.unf-user-input--moderator-email').hasClass('unf-user-input--isError')){
+        $('.unf-user-input--moderator-email').removeClass('unf-user-input--isError')
+        $('.unf-user-input--moderator-email .unf-user-input__info-msg').text('Please enter your email accounts in Tokopedia')
+      }
+
+      if ($('.create-gc__moderator').hasClass('create-gc__moderator--show')) {
+        $('.unf-user-input--moderator-email .unf-user-input__icon').removeClass('unf-user-input__icon--check');
+        $('.create-gc__moderator').removeClass('create-gc__moderator--show');
+      }
+      
+      // reset img
+      $('.unf-user-input__image-container').addClass('hide');
+      $('#gc-cover-img').removeAttr('src');
+      $('#input-gc-cover-img').val('');
+
+      handleDialogClose();
+    },
+  });
+
   $('#upload-gc-cover-img').on({
-    click: function () {
-      $('#input-gc-cover-img').click()
-    }
-  })
+    click: () => {
+      $('#input-gc-cover-img').click();
+    },
+  });
 
   $('#change-gc-cover-img').on({
-    click: function () {
-      $('#upload-gc-cover-img').click()
-    }
-  })
+    click: () => {
+      $('#upload-gc-cover-img').click();
+    },
+  });
 
   $('#delete-gc-cover-img').on({
-    click: function () {
-      $('.unf-user-input__image-container').addClass('hide')
-      $('#gc-cover-img').removeAttr('src')
-       $('#input-gc-cover-img').val('')
-    }
-  })
+    click: () => {
+      $('.unf-user-input__image-container').addClass('hide');
+      $('#gc-cover-img').removeAttr('src');
+      $('#input-gc-cover-img').val('');
+    },
+  });
 
   $('#input-gc-cover-img').on({
-    change: function () {
-      var imgType = ["image/png", "image/jpg", "image/jpeg"];
+    change: () => {
+      var imgType = ['image/png', 'image/jpg', 'image/jpeg'];
       if (this.files.length !== 0) {
-        if (this.files[0].type == imgType[0] || this.files[0].type == imgType[1] || this.files[0].type == imgType[2]) {
+        if (
+          this.files[0].type == imgType[0] ||
+          this.files[0].type == imgType[1] ||
+          this.files[0].type == imgType[2]
+        ) {
           if (this.files[0].size <= 10000000) {
-            isCover = true
+            isCover = true;
             readURL(this);
           } else {
-            $(this).val('')
-            handleOpenToaster(true, true, helper.image.error[0])
+            $(this).val('');
+            handleOpenToaster(true, true, helper.image.error[0]);
           }
-        }else{
-          handleOpenToaster(true, true, helper.image.error[1])
-          $(this).val('')
+        } else {
+          handleOpenToaster(true, true, helper.image.error[1]);
+          $(this).val('');
         }
       }
-      checkInputEmpty()
-    }
-  })
+      checkInputEmpty();
+    },
+  });
 
   $('#gc-name').on({
-    input: function () {
-      counterInput(this, '70')
-      if($(this).val()){
-        isName = true
-      }else{
-        isName = false
+    input: () => {
+      counterInput(this, '70');
+      if ($(this).val()) {
+        isName = true;
+      } else {
+        isName = false;
       }
-      checkInputEmpty()
-    }
-  })
+      checkInputEmpty();
+    },
+  });
 
   $('#gc-desc').on({
-    input: function () {
-      counterInput(this, '1000')
-      if($(this).val()){
-        isDesc = true
-      }else{
-        isDesc = false
+    input: () => {
+      counterInput(this, '1000');
+      if ($(this).val()) {
+        isDesc = true;
+      } else {
+        isDesc = false;
       }
-      checkInputEmpty()
-    }
-  })
+      checkInputEmpty();
+    },
+  });
 
   $('#moderator-email').on({
-    input: function () {
+    input: () => {
       // isModeratorEmail = true
       // checkInputEmpty()
     },
-    focus: function () {
-      handleInputError(inputEmail, 'Please enter your email accounts in Tokopedia', true)
+    focus: () => {
+      handleInputError(
+        inputEmail,
+        'Please enter your email accounts in Tokopedia',
+        true
+      );
     },
-    keypress: function () {
-      handleInputError(inputEmail, 'Please enter your email accounts in Tokopedia', true)
+    keypress: () => {
+      handleInputError(
+        inputEmail,
+        'Please enter your email accounts in Tokopedia',
+        true
+      );
       if (event.charCode == 13) {
         $('#check-moderator-email').click();
       }
-    }
-  })
+    },
+  });
 
   $('#check-moderator-email').on({
-    click: function () {
-      loadingCheckEmail(true)
+    click: () => {
+      loadingCheckEmail(true);
 
-      // if (!validateEmail(email)) {
-      //   setTimeout(() => {
-      //     loadingCheckEmail(false)
-      //   }, 500);
-      //   handleInputError(inputEmail, helper.email.error[0], false);
-      //   $('.create-gc__moderator').removeClass('create-gc__moderator--show')
-      //   return false;
-      // }
-
-      if (email.val() !== 'i') {
+      if (!validateEmail(email)) {
         setTimeout(() => {
           loadingCheckEmail(false)
         }, 500);
-        handleInputError(inputEmail, helper.email.error[1], false)
+        handleInputError(inputEmail, helper.email.error[0], false);
         $('.create-gc__moderator').removeClass('create-gc__moderator--show')
-        return false
+        return false;
+      }
+
+      if (email.val() !== 'ichsan.wahyudi@tokopedia.com') {
+        setTimeout(() => {
+          loadingCheckEmail(false);
+        }, 500);
+        handleInputError(inputEmail, helper.email.error[1], false);
+        $('.create-gc__moderator').removeClass('create-gc__moderator--show');
+        return false;
       }
 
       setTimeout(() => {
-        loadingCheckEmail(false)
-        $('.unf-user-input__icon').addClass('unf-user-input__icon--check')
-    
+        loadingCheckEmail(false);
+        $('.unf-user-input__icon').addClass('unf-user-input__icon--check');
       }, 500);
 
-      isModeratorEmail = true
-      isModeratorName = true
+      isModeratorEmail = true;
+      isModeratorName = true;
 
-      console.log(isModeratorEmail, isModeratorName)
-      checkInputEmpty()
-      
-      $('.customScrollBar--create-gc').animate({
-        scrollTop: 520
-      }, 1200)
+      // console.log(isModeratorEmail, isModeratorName);
+      checkInputEmpty();
 
+      $('.customScrollBar--create-gc').animate(
+        {
+          scrollTop: 520,
+        },
+        1200
+      );
 
-      $('.create-gc__moderator').addClass('create-gc__moderator--show')
-      
-    }
-  })
+      $('.create-gc__moderator').addClass('create-gc__moderator--show');
+    },
+  });
 
   // $('#moderator-name').on({
   //   input: function () {
@@ -155,15 +192,14 @@ $(document).ready(function () {
   //     checkInputEmpty()
   //   }
   // })
-
-})
+});
 
 function readURL(input) {
   var fileElem = document.getElementById(input.id).nextElementSibling;
   var previewImg = fileElem.children[0];
   if (input.files && input.files[0]) {
     var reader = new FileReader();
-    reader.onload = function (e) {
+    reader.onload = function(e) {
       previewImg.src = e.target.result;
     };
     $(fileElem).removeClass('hide');
@@ -173,14 +209,14 @@ function readURL(input) {
 }
 
 function counterInput(el, maxLength) {
-  var _self = $(el)
-  var length = _self.val().length
-  var counter = _self.next().find('.unf-user-input__info-counter')
-  counter.html(`${length}/${maxLength}`)
+  var _self = $(el);
+  var length = _self.val().length;
+  var counter = _self.next().find('.unf-user-input__info-counter');
+  counter.html(`${length}/${maxLength}`);
 }
 
 function checkInputEmpty() {
-  console.log(isCover, isName, isDesc, isModeratorEmail, isModeratorName)
+  // console.log(isCover, isName, isDesc, isModeratorEmail, isModeratorName);
   if (isCover && isName && isDesc && isModeratorEmail && isModeratorName) {
     $('#save-create-gc').attr('disabled', false);
   } else {
@@ -189,61 +225,73 @@ function checkInputEmpty() {
 }
 
 function initCustomSelect() {
-  $('select').each(function () {
+  $('select').each(function() {
     var $this = $(this),
       numberOfOptions = $(this).children('option').length;
 
-    var $selectedVal = this.options[this.selectedIndex].innerHTML
-    var status = this.className
+    var $selectedVal = this.options[this.selectedIndex].innerHTML;
+    var status = this.className;
 
     $this.wrap('<div class="unf-user-select"></div>');
-    $this.after(`<div class="unf-user-select__selected ${this.className}"><div class="unf-user-select__selected-arrow"></div><span></span></div>`);
+    $this.after(
+      `<div class="unf-user-select__selected ${
+        this.className
+      }"><div class="unf-user-select__selected-arrow"></div><span></span></div>`
+    );
     var $styledSelect = $this.next('div.unf-user-select__selected');
     $styledSelect.children('span').text($selectedVal);
-    
 
     var $list = $('<ul />', {
-      'class': 'unf-user-select__options'
+      class: 'unf-user-select__options',
     }).insertAfter($styledSelect);
 
     for (var i = 0; i < numberOfOptions; i++) {
       $('<li />', {
-        text: $this.children('option').eq(i).text(),
-        rel: $this.children('option').eq(i).val()
+        text: $this
+          .children('option')
+          .eq(i)
+          .text(),
+        rel: $this
+          .children('option')
+          .eq(i)
+          .val(),
       }).appendTo($list);
     }
 
     var $listItems = $list.children('li');
 
-    $styledSelect.click(function (e) {
+    $styledSelect.click(function(e) {
       e.stopPropagation();
-      $('div.unf-user-select__selected.unf-user-select__selected--open').not(this).each(function () {
-        $(this).removeClass('unf-user-select__selected--open')
-      });
-      $(this).toggleClass('unf-user-select__selected--open')
+      $('div.unf-user-select__selected.unf-user-select__selected--open')
+        .not(this)
+        .each(function() {
+          $(this).removeClass('unf-user-select__selected--open');
+        });
+      $(this).toggleClass('unf-user-select__selected--open');
     });
 
-    $listItems.click(function (e) {
+    $listItems.click(function(e) {
       e.stopPropagation();
       $this.val($(this).attr('rel'));
-      // $this.change()
-      console.log(status)
-      
-      if (status === 'channel__status-select'){
-        console.log('masuk')
-        
+
+      if (status === 'channel__status-select') {
+        if ($selectedVal !== $this.val()) {
+          $this.change();
+          return;
+        }
       }
-      
-      $styledSelect.removeClass('unf-user-select__selected--open').children('span').text($(this).text());
+
+      $styledSelect
+        .removeClass('unf-user-select__selected--open')
+        .children('span')
+        .text($(this).text());
       // $list.hide();
       //console.log($this.val());
     });
 
-    $(document).click(function () {
+    $(document).click(function() {
       $styledSelect.removeClass('unf-user-select__selected--open');
       // $list.hide();
     });
-
   });
 }
-
