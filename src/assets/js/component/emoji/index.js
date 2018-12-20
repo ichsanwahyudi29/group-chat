506c9f9982590f34894090d40104b202801035cc
Fajarma <fajarma@github.com>
2018-12-20 18:03:47 +0700

    add emoji.json

--- src/assets/js/component/emoji/index.js
+++ src/assets/js/component/emoji/index.js
@@ -1,6 +1,20 @@
 $(document).ready(function(){
     // 1 - 80 emoji
-    for(var i = 0; i < 80; i++){
+
+    loadJSON('./assets/js/component/emoji/emoji.json', function (response) {
+        var res = JSON.parse(response);
+        dataEmoji = res.emoji
+        dataEmoji.map(item => {
+
+            var emoji = `
+            <a class="emoji-outer">
+                <span class="emoji-icon">${item}</span>
+            </a>`;
+            $('.unf-user-input__emoji-content').append(emoji);
+        })
+    })
+
+    /* for(var i = 0; i < 80; i++){
         var iString = i.toString(16)
         if(iString.length < 2){
             iString = '0'+iString
@@ -13,7 +27,7 @@ $(document).ready(function(){
             <span class="emoji-icon">&#x1F6${iString}</span>
         </a>`;
         $('.unf-user-input__emoji-content').append(emoji);
-    }
+    } */
 
     $(function handleEmoji() {
         $(document).click(function(e){
